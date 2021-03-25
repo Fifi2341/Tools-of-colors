32373e7e8877d290253e6df81ce7a0f082fe0633
Fifi2341 <69726272+Fifi2341@users.noreply.github.com>
2021-03-25 19:00:34 +0100

    Update Spikeball.cs

--- Npcs/TheBigAssShield/Spikeball.cs
+++ Npcs/TheBigAssShield/Spikeball.cs
@@ -3,7 +3,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 
 using Microsoft.Xna.Framework;
-using TPUG.Projectiles;
+using ColorSwords.Projectiles;
 
 namespace Colorswords.Npcs.TheBigAssShield
 {

46b70826640d48ab3aefef619b90b35f53216f9c
Fifi2341 <69726272+Fifi2341@users.noreply.github.com>
2021-03-25 19:08:40 +0100

    Update GreencandycanearmorL.cs

--- Items/Armors/TPUGarmor/GreencandycanearmorL.cs
+++ Items/Armors/TPUGarmor/GreencandycanearmorL.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 using Terraria.ID;
-using ColorSwords.TPUG_items;
+using ColorSwords.Items.TPUG_items;
 
 namespace Colorswords.Items.Armors.TPUGarmor
 {
